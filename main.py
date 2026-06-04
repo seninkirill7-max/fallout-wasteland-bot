23965a3b29c2e858478aadab38910eb2736fcfb7
seninkirill7-max <seninkirill7@gmail.com>
2026-06-04 19:33:28 +0800

    Update main.py

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ from aiogram import Bot, Dispatcher, F
 from aiogram.types import Message
 from aiogram.utils.keyboard import ReplyKeyboardBuilder
 
+from database import create_player, get_player
+
 BOT_TOKEN = os.getenv(“BOT_TOKEN”)
 
 bot = Bot(token=BOT_TOKEN)
@@ -24,21 +26,60 @@ return builder.as_markup(resize_keyboard=True)
 
 @dp.message(F.text == “/start”)
 async def start(message: Message):
+create_player(message.from_user.id)
+
 await message.answer(
-“☢ ПУСТОШИ ☢\n\nДобро пожаловать в Пустоши.”,
-reply_markup=main_menu()
+    "☢ ПУСТОШИ ☢\n\nДобро пожаловать в Пустоши.",
+    reply_markup=main_menu()
 )
 
 @dp.message(F.text == “📊 Статистика”)
 async def stats(message: Message):
+player = get_player(message.from_user.id)
+
 await message.answer(
-“📊 Статистика персонажа\n\n”
-“Уровень: 1\n”
-“Опыт: 0\n”
-“Здоровье: 100\n”
-“Броня: 0\n”
-“Выносливость: 100\n”
-“Уровень оружия: 1”
+    f"📊 Статистика персонажа\n\n"
+    f"Уровень: {player[1]}\n"
+    f"Опыт: {player[2]}\n"
+    f"❤️ Здоровье: {player[3]}\n"
+    f"🛡 Броня: {player[4]}\n"
+    f"⚡ Выносливость: {player[5]}\n"
+    f"🔫 Уровень оружия: {player[6]}\n\n"
+    f"💴 Доллары КНР: {player[7]}\n"
+    f"🎟 Облигации: {player[8]}\n"
+    f"⚛ Атомные крышки: {player[9]}"
+)
+
+@dp.message(F.text == “🎒 Инвентарь”)
+async def inventory(message: Message):
+await message.answer(
+“🎒 Инвентарь\n\n”
+“Склад ресурсов пока пуст.”
+)
+
+@dp.message(F.text == “☢ Выход в Пустоши”)
+async def wasteland(message: Message):
+await message.answer(
+“☢ Выберите доступную локацию.\n\n”
+“🪨 Окрестности Убежища (1 уровень)”
+)
+
+@dp.message(F.text == “🏋 Тренировочный зал”)
+async def gym(message: Message):
+await message.answer(
+“🏋 Для входа необходимы Облигации.”
+)
+
+@dp.message(F.text == “🛒 Торговая лавка”)
+async def shop(message: Message):
+await message.answer(
+“🛒 Торговая лавка пока закрыта.”
+)
+
+@dp.message(F.text == “🏛 Обменный бункер”)
+async def bunker(message: Message):
+await message.answer(
+“🏛 Здесь можно будет обменивать Атомные крышки.”
 )
 
 async def main():
